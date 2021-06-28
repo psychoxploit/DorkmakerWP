baf0120366aa5c64c38554c118c19834269bcb21
PsychoXploit <61023708+psychoxploit@users.noreply.github.com>
2021-06-28 15:05:57 +0700

    Add files via upload

---
+++ DorkmakerPX.py
@@ -0,0 +1,38 @@
+import os
+os.system('pip install requests')
+import requests, re
+print ('''
+
+ ███████████                              █████               █████ █████           ████            ███   █████   
+░░███░░░░░███                            ░░███               ░░███ ░░███           ░░███           ░░░   ░░███    
+ ░███    ░███  █████  █████ ████  ██████  ░███████    ██████  ░░███ ███   ████████  ░███   ██████  ████  ███████  
+ ░██████████  ███░░  ░░███ ░███  ███░░███ ░███░░███  ███░░███  ░░█████   ░░███░░███ ░███  ███░░███░░███ ░░░███░   
+ ░███░░░░░░  ░░█████  ░███ ░███ ░███ ░░░  ░███ ░███ ░███ ░███   ███░███   ░███ ░███ ░███ ░███ ░███ ░███   ░███    
+ ░███         ░░░░███ ░███ ░███ ░███  ███ ░███ ░███ ░███ ░███  ███ ░░███  ░███ ░███ ░███ ░███ ░███ ░███   ░███ ███
+ █████        ██████  ░░███████ ░░██████  ████ █████░░██████  █████ █████ ░███████  █████░░██████  █████  ░░█████ 
+░░░░░        ░░░░░░    ░░░░░███  ░░░░░░  ░░░░ ░░░░░  ░░░░░░  ░░░░░ ░░░░░  ░███░░░  ░░░░░  ░░░░░░  ░░░░░    ░░░░░  
+                       ███ ░███   Code by Shirgil & GHz                   ░███                                    
+                      ░░█████ [github: https://github.com/psychoxploit]  █████                                   
+                       ░░░░░░                                            ░░░░░   
+
+''')
+r = requests.session()
+def memek(kontol):
+    for keyword in kontol:
+        keyword = keyword.strip()
+        result = open('wd.txt', 'r').read().splitlines()
+        for res in result:
+            res = res.strip()
+            results = res+' \t'+keyword
+            print(results)
+            with open('Dork.txt','a+') as xn:
+                xn.write(results+'\n')
+
+if __name__ == "__main__":
+	try:
+		xnx = open(input('list:'),'r',encoding='utf-8').read().splitlines()
+		memek(xnx)
+	except Exception as w:
+		print(w)
+
+input("ALL DONE")
